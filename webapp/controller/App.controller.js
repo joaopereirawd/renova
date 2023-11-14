e06e50b602b350515a6f24ec33a0beacbac9c45d
joaopereira <joaopereirawd@gmail.com>
2023-11-14 20:00:48 +0000

    Add base application

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Button"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Button) {
         "use strict";
 
         return Controller.extend("renova.controller.App", {
             onInit: function () {
 
-            }
+            },
+
+
         });
     });
